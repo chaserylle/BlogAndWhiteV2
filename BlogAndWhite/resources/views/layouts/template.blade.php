7a05a5418a0347b2a4554f21b0119036b4dc70dc
LovelynParis <32500236+ParisLovelyn@users.noreply.github.com>
2018-06-26 17:00:35 +0800

    #39 footer

--- BlogAndWhite/resources/views/layouts/template.blade.php
+++ BlogAndWhite/resources/views/layouts/template.blade.php
@@ -10,7 +10,9 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-<body>
+<body style="padding-bottom:10%">
+
+
     @section('navbar')
      <div class="navig"> 
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -40,8 +42,41 @@
       </nav>
     </div>
 
+
     <div class="container">
       @yield('content')
+
     </div>
-    </body>
-</html>
+
+
+    
+    @section('footer')
+    <style>
+    .footer {
+        position: fixed;
+        bottom: 0%;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: 9%;
+        background-color: #f5f5ef;
+        color: black;
+        text-align: center;
+        font-size: 2vh;
+        float: bottom;
+    }
+
+    </style>
+    <div class="text-muted footer">
+      <span>
+        Blog & White <br/>
+        Aika Vien Dayrit |
+          Chaserylle Know-well Sison | 
+          Lovelyn Paris,
+          <br/>
+          2018
+      </span>
+    </div>
+        
+        </body>
+    </html>
