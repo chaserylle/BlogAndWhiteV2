bf2e1225e611cd7e15fedf23eeabbffd8b041c15
LovelynParis <32500236+ParisLovelyn@users.noreply.github.com>
2018-06-26 16:57:15 +0800

    #20 Displays authors' blogs

--- BlogAndWhite/resources/views/home.blade.php
+++ BlogAndWhite/resources/views/home.blade.php
@@ -1,20 +1,39 @@
-<!-- @extends('app')
+@extends('layouts.template')
+
+@section('title', 'Home')
 
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
 
-				<div class="panel-body">
-					You are logged in!
-				</div>
+<div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+	<div class="col-md-12 px-0 headerblog">
+		<h1 class="display-4 font-italic">Blog&White</h1>
+		<p class="lead my-3">Share. Relive. Experience.</p>
+	</div>
+</div>
+
+	<div class="row mb-2">
+	
+	@foreach ($posts as $post)
+	<div class="col-md-6">
+		<div class="card flex-md-row mb-4 box-shadow h-md-250 bg-light">
+			<div class="card-body d-flex flex-column align-items-start">
+				<strong class="d-inline-block mb-2 text-primary">{{ $post->username }}</strong>
+				<h3 class="mb-0">
+					<a href=post/getpost.php?post_id='.$postid.' class="text-dark">{{ $post->title }}</a>
+				</h3>
+				<div class="mb-1 text-muted">{{ $post->date_published }}</div>
+				<!--<p class="card-text mb-auto">'.limitTextWords($content, 50, true, true).'</p>-->
+				<form action="#" method="GET">
+					<input type="hidden" name="post_id" value="'.$postid.'">
+					<button type="submit" class="btn btn-outline-dark">Continue Reading</a></button>
+				</form>
 			</div>
 		</div>
 	</div>
-</div>
-@endsection
- --> 
 
- <h1>home</h1>
+
+
+		@endforeach
+	</div>
+
+	@stop
